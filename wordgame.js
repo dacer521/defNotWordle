49716530ed03ca80d6acdbbb1223a4e1d1002c13
Bell <lbell1@wpi.edu>
2024-07-25 13:12:03 -0400

    fixed console errors; renamed script.js to wordgame.js

---
+++ wordgame.js
@@ -0,0 +1,36 @@
+let popularWords;
+let allWords;
+
+function loadGame() {
+    Promise.all([
+        fetch('./popular.txt')
+            .then(response => response.text())
+            .then(text => {
+                popularWords = text.split("\r\n");
+                popularWords.pop(); // Remove the '' at the end
+            })
+            .catch(error => {
+                console.error('Error fetching words: ', error);
+            }),
+        fetch("./enable1.txt")
+            .then(response => response.text())
+            .then(text => {
+                allWords = text.split("\r\n");
+                allWords.pop(); // Remove the '' at the end
+            })
+            .catch(error => {
+                console.error('Error fetching words: ', error);
+            })]).finally(wordsLoaded);
+}
+
+function wordsLoaded() {
+    console.log(`Loaded ${popularWords.length} popular words and ${allWords.length} dictionary words!`)
+}
+
+// TODO: copy randInt
+// TODO: write function isWord(word)
+
+// For checking word:  json.hasOwnProperty("programming")
+// For array of words: let arr = Object.keys(json)
+// For a random word:  let word = arr[randInt(0, arr.length - 1)];
+    
